d3288158be81bb206d22e8c3da98102230ea033a
Minchan <78589751+minchjung@users.noreply.github.com>
2021-09-08 20:17:44 +0900

    SQL TWO WHERE CLAUSE WITH HAVING

---
+++ mySql copy.sql
@@ -0,0 +1,18 @@
+
+# PLACES 테이블은 공간 임대 서비스에 등록된 공간의 정보를 담은 테이블입니다. PLACES 테이블의 구조는 
+# ID, NAME, HOST_ID는 각각 공간의 아이디, 이름, 공간을 소유한 유저의 아이디를 나타냅니다. 
+# ID는 기본키입니다.
+
+# 이 서비스에서는 공간을 둘 이상 등록한 사람을 "헤비 유저"라고 부릅니다. 
+# 헤비 유저가 등록한 공간의 정보를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+
+-- 코드를 입력하세요
+SELECT `ID`, `NAME`, `HOST_ID`
+FROM `PLACES`
+WHERE `HOST_ID` IN (
+    SELECT `HOST_ID`
+    FROM `PLACES`
+    GROUP BY `HOST_ID`
+    HAVING COUNT(`HOST_ID`) >= 2
+    )
+ORDER BY `ID`;
